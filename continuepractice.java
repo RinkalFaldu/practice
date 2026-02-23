e19f3fc4229722f823476c021383a7a67780b7ad
RinkalFaldu <rinkal1faldu@gmail.com>
2026-02-23 13:17:20 -0800

    practice of break and continue key worde

---
+++ continuepractice.java
@@ -0,0 +1,11 @@
+//continue used for skipping the current iteration not breaking the loop
+public class continuepractice {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            if (i == 3) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
