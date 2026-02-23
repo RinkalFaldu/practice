4289686626bf81018683ef12c360e3aea3390135
RinkalFaldu <rinkal1faldu@gmail.com>
2026-02-23 12:55:29 -0800

    this problem reverse number without printing it

---
+++ riversesamenum.java
@@ -0,0 +1,13 @@
+// in this problem i am reverse number  without printing reverse number
+public class riversesamenum{
+    public static void main(String[] args) {
+        int n = 10899;
+        int rev = 0;
+        while (n > 0) {
+            int lastDigit = n % 10;
+            rev = rev * 10 + lastDigit;
+            n = n / 10;
+        }
+        System.out.println(rev);
+    }
+}
