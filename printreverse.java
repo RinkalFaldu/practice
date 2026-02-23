f059bf7a9fe678d6be82b5dcef0565a481979c78
RinkalFaldu <rinkal1faldu@gmail.com>
2026-02-23 12:40:48 -0800

    push reverse number problem

---
+++ printreverse.java
@@ -0,0 +1,15 @@
+// in this problem i am going to print the reverse of a string
+// if i want to last digit of number do num % 10
+// to remove last digit of number do num / 10
+ 
+public class printreverse {
+    public static void main(String[] args) {
+        int num = 10899;
+        while(num > 0){
+           int lastDigit = num % 10;        //find last digit
+           System.out.print(lastDigit);
+           num = num / 10;                   //remove last digit
+        }
+       System.out.println();
+    }
+}
