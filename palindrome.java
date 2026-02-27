851708e9a9e892e8a24b8a799cedf54292460a41
RinkalFaldu <rinkal1faldu@gmail.com>
2026-02-27 12:23:07 -0800

    done changes

--- palindrome.java
+++ palindrome.java
@@ -1,3 +1,23 @@
 public class palindrome {
     
+
+    public static void isPalindrome (int number){
+        int mynum = number;
+        int reverse = 0;
+        while (number > 0){
+        int lastdigit = number /10;
+        reverse = reverse * 10 + lastdigit;
+        number = number/10;
+        }
+        if (number == reverse){
+            System.out.println(mynum + " is a palindrom");
+        }
+        else{
+            System.out.println(mynum + " is not palindrom");
+        }
+    }
+    public static void main(String[] args) {
+          isPalindrome(321);
+    
+    }
 }
