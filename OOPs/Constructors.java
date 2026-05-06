678aa8ebb88a03f3d8a52cd614d2f80c4970a0e9
RinkalFaldu <rinkal1faldu@gmail.com>
2026-05-05 17:04:08 -0700

    done OOPs

---
+++ OOPs/Constructors.java
@@ -0,0 +1,29 @@
+public class Constructors {
+    public static void main(String[] args) {
+        Student s1 = new Student ();
+        Student s2 = new Student ("Rinkal");
+        Student s3 = new Student(123);
+        
+    }
+    
+}
+
+class Student {
+    String name;
+    int roll;
+    // constructor
+    // Non-parameterized
+    Student (){
+        System.out.println("Constructor is called----");
+    }
+    //parameterized
+    Student (String name){
+        this.name = name;
+
+    }
+
+    Student (int roll){
+        this.roll = roll;
+    }
+
+}
---
+++ OOPs/OOPS.java
@@ -0,0 +1,45 @@
+public class OOPS {
+    public static void main(String[] args) {
+        Pen p1 = new Pen();          // constructure. special function
+        p1.setColor("Blue");
+        System.out.println(p1.getColor());
+
+        p1.setTip (5);
+        System.out.println(p1.getTip());
+
+        p1.setColor("Yellow");
+        System.out.println(p1.getColor());
+
+    }
+
+}
+class Pen {
+    private String color;
+    private int tip;
+
+    String getColor (){
+        return this.color;
+    }
+
+    int getTip(){
+        return this.tip;
+    }
+
+    void setColor (String newColor){
+        color = newColor;
+    }
+
+    void setTip (int newTip){
+        tip = newTip;
+    }
+}
+
+class Student {
+    String name;
+    int age;
+    float percentage;
+
+    void calcPercentage (int phy, int chem, int math){
+        percentage = (phy + chem + math)/3;
+    }
+}
