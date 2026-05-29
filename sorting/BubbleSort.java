9504a5fcda2febf5788468fccccafd623e0ff3ee
RinkalFaldu <rinkal1faldu@gmail.com>
2026-05-29 15:03:26 -0700

    done space and time complexity concept

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -15,7 +15,27 @@ public class BubbleSort {
             }
         }
     }
+     
+    // best case time complexity is O(n) for best case
+    public static void Optimizebubblesort (int arr[]){
+        
+        for (int turn =0; turn < arr.length -1; turn++){
+            boolean swapped = false;
+            for ( int j =0; j < arr.length-1-turn; j++){
+                if (arr [j] > arr [j+1]){
+                    //swap
+                    int temp = arr [j];
+                    arr [j] = arr [j+1];
+                    arr [j+1] = temp;
+                    swapped = true;
 
+                }
+            }
+            if (swapped == false){
+                break;
+            }
+        }
+    }
     // function to print array
     public static void printArr (int arr[]){
         for ( int i =0; i< arr.length; i++){
