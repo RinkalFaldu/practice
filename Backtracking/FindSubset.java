982038dc79a61c3faf4bb0bc78c3f5f0b1654582
RinkalFaldu <rinkal1faldu@gmail.com>
2026-06-02 15:38:37 -0700

    done problem of backtracking

---
+++ Backtracking/FindSubset.java
@@ -0,0 +1,29 @@
+package Backtracking;
+
+public class FindSubset {
+    public static void findSubSets(String str, String ans, int i){
+        //base case
+        if(i == str.length()){
+            if (ans.length() ==0){
+                System.out.println("null"); // this prints last subset as null
+            }
+            else {
+            System.out.println(ans);
+        
+            }
+            return;
+        }
+        //recursion
+        //yes choice
+        findSubSets(str, ans+str.charAt(i), i+1);
+        //No choices
+        findSubSets(str, ans, i+1);
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        findSubSets(str, "", 0);
+        
+    }
+    
+}
