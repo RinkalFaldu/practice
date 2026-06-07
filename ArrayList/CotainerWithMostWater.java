880ea828aec2dcceef5aa7f5fd2ae7267ab59111
RinkalFaldu <rinkal1faldu@gmail.com>
2026-06-07 15:12:23 -0700

    done more problem of ArrayList

--- ArrayList/CotainerWithMostWater.java
+++ ArrayList/CotainerWithMostWater.java
@@ -1,10 +1,34 @@
 package ArrayList;
 import java.util.ArrayList;
-
+// this is brute force -O(n^2)
 
 public class CotainerWithMostWater {
-    public static void main(String[] args) {
-        
+
+    public static int getmaxWater (ArrayList<Integer> height){
+        int maxWater = 0;
+        for (int i =0; i<height.size(); i++){
+            for (int j = i+1; j<height.size(); j++){
+                int width = j -i;
+                int minheight =Math.min(height.get(i), height.get(j));
+                int waterArea = minheight * width;
+                maxWater = Math.max(maxWater, waterArea);
+            }
+        }
+        return maxWater;
     }
+    public static void main(String[] args) {
+        ArrayList<Integer> height = new ArrayList<>();
+        height.add(1);
+        height.add(8);
+        height.add(6);
+        height.add(2);
+        height.add(5);
+        height.add(4);
+        height.add(8);
+        height.add(3);
+        height.add(7);
+
+        System.out.println(getmaxWater(height));
+     }
     
 }
---
+++ ArrayList/OptmizeContainerWithMostWater.java
@@ -0,0 +1,42 @@
+package ArrayList;
+import java.util.ArrayList;
+
+// time complexity O(n)
+
+public class OptmizeContainerWithMostWater {
+    public static int getMaxWater (ArrayList<Integer> height){
+        int left = 0;
+        int right = height.size() -1;
+        int maxWater =0;
+
+        while (left < right){
+            int width = right - left;
+            int minheight =Math.min(height.get(left), height.get(right));
+            int waterArea = minheight * width;
+            maxWater = Math.max(maxWater, waterArea);
+
+            if (height.get(left)< height.get(right)){
+                left++;
+            } 
+            else {
+                right--;
+            }
+        }
+     return maxWater;    
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> height = new ArrayList<>();
+        height.add(1);
+        height.add(8);
+        height.add(6);
+        height.add(2);
+        height.add(5);
+        height.add(4);
+        height.add(8);
+        height.add(3);
+        height.add(7);
+
+        System.out.println(getMaxWater(height));
+    }
+    
+}
