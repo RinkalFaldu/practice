8ed4271dd4c6022ed5688e1d96ea878c96d1adaa
RinkalFaldu <rinkal1faldu@gmail.com>
2026-06-09 16:18:12 -0700

    done stack problems

--- Stack/StackLinkedList.java
+++ Stack/StackLinkedList.java
@@ -1,6 +1,6 @@
 package Stack;
 
-import java. util.ArrayList;
+import java. util.LinkedList;
 import java.util.Stack;
 
 
@@ -14,7 +14,34 @@ public class StackLinkedList {
         }
     }
     static class stack {
-        
+        static Node head = null;
+        public static boolean isEmpty(){
+            return head == null;
+        }
+        public static void push (int data){
+            Node newNode = new Node (data);
+            if (isEmpty()){
+                head = newNode;
+                return;
+
+            }
+            newNode.next = head;
+            head = newNode;
+        }
+        public static int pop(){
+            if(isEmpty()){
+                return -1;
+            }
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+        public static int peek(){
+            if(isEmpty()){
+                return -1;
+            }
+            return head.data;
+        }
     }
     public static void main(String[] args) {
         Stack s = new Stack();
---
+++ Stack/StockSpanProblem.java
@@ -0,0 +1,10 @@
+package Stack;
+import java.util.Stack;
+
+// max no of consecutive days for which price <= today's price
+
+public class StockSpanProblem {
+    public static void main(String[] args) {
+        
+    }
+}
