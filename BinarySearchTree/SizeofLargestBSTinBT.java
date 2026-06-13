687a70eb61b42bf99d318a674667f3e52c7afce2
RinkalFaldu <rinkal1faldu@gmail.com>
2026-06-12 17:04:26 -0700

    done problems of bst

---
+++ BinarySearchTree/SizeofLargestBSTinBT.java
@@ -0,0 +1,27 @@
+package BinarySearchTree;
+
+public class SizeofLargestBSTinBT {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+        Node (int data){
+            this.data = data;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node (50);
+        root.left = new Node (30);
+        root.left.left = new Node (5);
+        root.left.right = new Node (20);
+        root.right = new Node (60);
+        root.right.right = new Node (70);
+        root.right.left = new Node (45);
+        root.right.right.right = new Node (80);
+        root.right.right.left = new Node (65);
+
+        
+    }
+    
+}
