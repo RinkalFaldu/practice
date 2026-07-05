4c496716ae33deb38fdd6bd22c0c443be93f3959
RinkalFaldu <rinkal1faldu@gmail.com>
2026-07-05 13:05:14 -0700

    done segment Tree

--- SegmentTree/CountAndMeaningofNodes.java
+++ SegmentTree/CountAndMeaningofNodes.java
@@ -18,7 +18,7 @@ package SegmentTree;
 public class CountAndMeaningofNodes {
     static int tree[];
     public static void init(int n){
-        tree = new int [4*n];
+        tree = new int [2*n-1];
     }
     public static int buildST(int arr[], int i, int start, int end){
        if (start == end){
@@ -51,6 +51,26 @@ public class CountAndMeaningofNodes {
        return getSumUtil(0, 0, n-1, qi, qj);
 
     }
+    public static void updateUtil (int i, int si, int sj, int idx, int diff ){
+        if (idx > sj || idx < si){
+            return;
+        }
+        tree[i] += diff;
+        if (si != sj){ // non-leaf
+           int mid = (si + sj)/2;
+           updateUtil(2*i+1, si, mid, idx, diff); // left
+           updateUtil(2*i+2, mid+1,sj, idx, diff ); // right
+        }
+         
+
+    }
+    public static void update (int arr[], int idx, int newVal){
+        int n = arr.length;
+        int diff = newVal - arr[idx];
+        arr[idx] = newVal;
+        updateUtil(0, 0, n-1, idx, diff);
+
+    }
     public static void main (String args[]){
         int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
         int n = arr.length;
@@ -60,6 +80,8 @@ public class CountAndMeaningofNodes {
             System.out.print(tree[i] + " ");   
         }
         System.out.println();
-      System.out.println( getSum(arr, 2, 5));
+        System.out.println( getSum(arr, 2, 5));// 18
+        update(arr, 2, 2);
+        System.out.println(getSum(arr,2, 5 )); // 17
     }
 }
