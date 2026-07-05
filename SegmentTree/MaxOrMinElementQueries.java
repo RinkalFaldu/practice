2b8c120493a4c9ac672fef61b28d91e9f62ef5f2
RinkalFaldu <rinkal1faldu@gmail.com>
2026-07-05 16:30:10 -0700

    done max min ST

--- SegmentTree/MaxOrMinElementQueries.java
+++ SegmentTree/MaxOrMinElementQueries.java
@@ -15,6 +15,29 @@
         buildTree(2*i+2, mid+1, sj, arr);
         tree[i] = Math.max(tree[2*i+1], tree[2*i+2]);
     }
+    public static int getMax(int arr[], int qi, int qj){
+        int n = arr.length;
+        return getMaxUtil(0, 0, n-1, qi, qj);
+
+    }
+    public static int getMaxUtil(int i, int si, int sj, int qi, int qj){
+        if (si > qj || sj < qi ){
+            return Integer.MIN_VALUE;
+        }
+        else if(si >= qi && sj <= qj) {
+            return tree[i];
+
+        } 
+        else {
+            int mid = (si + sj)/2;
+            int lefAns = getMaxUtil(2*i+1,si, mid, qi, qj );
+            int rightAns = getMaxUtil(2*i+2,mid+1, sj, qi, qj );
+            return Math.max(lefAns, rightAns);
+
+        }
+
+    }
+    
     public static void main (String args[]){
         int arr[] = {6, 8, -1, 2, 17, 1, 3, 2, 4};
         int n = arr.length;
@@ -23,5 +46,8 @@
         for (int i =0; i<tree.length; i++){
             System.out.print(tree[i] + " ");
         }
+        System.out.println();
+       int max =  getMax(arr, 2, 5);
+       System.out.println(max);
     }
  }
