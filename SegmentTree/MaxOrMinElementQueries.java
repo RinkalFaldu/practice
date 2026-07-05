0e40090c6fb90108e6e3bd6eb1ab0431e7a79856
RinkalFaldu <rinkal1faldu@gmail.com>
2026-07-05 16:42:45 -0700

    done problem of ST

--- SegmentTree/MaxOrMinElementQueries.java
+++ SegmentTree/MaxOrMinElementQueries.java
@@ -37,7 +37,25 @@
         }
 
     }
-    
+    public static void updateUtil (int i, int si, int sj, int idx, int newVal ){
+        if (idx > sj || idx < si){
+            return;
+        }
+       tree[i] = Math.max(tree[i], newVal);
+       if (si != sj){
+            int mid = (si + sj)/2;
+            updateUtil(2*i+1, si, mid, idx, newVal);
+            updateUtil(2*i+2, mid+1, sj, idx, newVal);
+        }
+    }
+    public static void update (int arr[], int idx, int newVal){
+        
+        arr[idx] = newVal;
+        int n = arr.length;
+        updateUtil(0, 0, n-1, idx, newVal);
+
+    }
+
     public static void main (String args[]){
         int arr[] = {6, 8, -1, 2, 17, 1, 3, 2, 4};
         int n = arr.length;
@@ -48,6 +66,9 @@
         }
         System.out.println();
        int max =  getMax(arr, 2, 5);
-       System.out.println(max);
+       System.out.println(max); // 17
+       update(arr, 2, 20);
+       max =  getMax(arr, 2, 5);
+       System.out.println(max); // 20
     }
  }
