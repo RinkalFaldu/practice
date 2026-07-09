b4e65cb38930efdf70a7deddc7226f97f7fc5acf
RinkalFaldu <rinkal1faldu@gmail.com>
2026-07-09 15:32:24 -0700

    doing problem trie

---
+++ Tries/StartsWithProblem.java
@@ -0,0 +1,10 @@
+package Tries;
+// Create a function boolean starts with (String prefix) for a trie.
+// return true if there is previously inserted string word that has the prefix, and false otherwise.
+// words [] = {"apple", "app", "mango", "man", "woman"}
+// prefix = "app" output = true;   in list there is word start with app
+// prefix = "moon" output = false;
+
+public class StartsWithProblem {
+    
+}
